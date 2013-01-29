c55934c611ecee8f28fc095dd00d72b4bf1d726c
Romain Brault <rb812@project05.doc.ic.ac.uk>
2013-01-29 13:07:00 +0000

    corrected compilation error

---
+++ build/CMakeFiles/HDRimage.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/HDRimage.dir/src/main.cpp.o"
+  "CMakeFiles/HDRimage.dir/src/HDRimage.cpp.o"
+  "HDRimage.pdb"
+  "HDRimage"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/HDRimage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
