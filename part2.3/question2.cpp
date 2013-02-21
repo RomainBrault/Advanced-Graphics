c4df96dd581d9f6e9dd19a9b2457ab41d0f114b2
Romain Brault <romain@rb-fedora.workstation>
2013-02-21 07:05:14 +0000

    argv part2.3

--- part2.3/question2.cpp
+++ part2.3/question2.cpp
@@ -11,26 +11,29 @@ using namespace obj;
 
 int main(int argc, char** argv) {
 
-    if (argc < 3)
+    if ( argc < 6 ) {
 	return -1;
+    }
 
-    Phong p(atof(argv[1]), 0.4, 0.6);
-    int nb_samples = atoi(argv[2]);
-    p.generateSamples(nb_samples);
-
+    uint32_t nb_samples = std::atoi( argv[ 3 ] );
     image latlong;
-    latlong.loadPFM("../CO417-HW1/GraceCathedral/grace_latlong.pfm");
+    latlong.loadPFM(string( argv[ 1 ] ) );
+    if ( latlong.isEmpty( ) == true ) {
+        return -1;
+    }
+
+    Phong p( std::atof( argv[ 2 ] ), 0.4, 0.6 );
+    p.generateSamples(nb_samples);
     image tmp = latlong;
 
     for (int i = 0; i < nb_samples; ++i) {
-	uint32_t theta = p.getTheta(i, tmp.getHeight());
-	uint32_t phi = p.getPhi(i, tmp.getWidth());
-	
-	tmp.circleFilled( sphere(2, phi, theta) , 0.0, 1.0, 0.0);
+	uint32_t theta = p.getTheta( i, tmp.getHeight( ) );
+	uint32_t phi = p.getPhi( i, tmp.getWidth( ) );
+	tmp.circleFilled( sphere( 2, phi, theta ) , 0.0, 1.0, 0.0);
     }
-    tmp.linearToneMap( 9.0 );
-    tmp.gamma( 2.2 );
-    tmp.normalise(255);
-    tmp.savePNM("test.ppm", SFMT);
+    tmp.linearToneMap( std::atof( argv[ 4 ] ) );
+    tmp.gamma(std::atof( argv[ 5 ] ) );
+    tmp.normalise( 255 );
+    tmp.savePNM( "test.ppm", SFMT );
 
 }
