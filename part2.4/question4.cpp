7172595a8277b98054caa3572078aad8f11dc726
Jérémy RIVIERE <jeremy.riviere@etu.enseeiht.fr>
2013-02-25 02:42:53 +0000

    Crawling with the ray casting :D

--- part2.4/question4.cpp
+++ part2.4/question4.cpp
@@ -43,6 +43,8 @@ int main(int argc, char** argv) {
 
     /* Add code here. */
 
+    temp.renderIS(s, view, latlong, brdf::model(view, 1.0, 0.0, 1.0), points, n_points);
+
     temp.linearToneMap( stops );
     temp.gamma( gamma );
     temp.normalise( 255 );
--- src/HDRimage.hpp
+++ src/HDRimage.hpp
@@ -4,6 +4,7 @@
 #include <AdvancedGraphicsConfig.hpp>
 #include <Random.hpp>
 #include <Sphere.hpp>
+#include <BRDF.hpp>
 
 #define MIN( x, y ) ( x ) < ( y ) ? ( x ) : ( y )
 
@@ -233,6 +234,8 @@ public:
         obj::sphere const &, obj::vect< float, 3 > const &, image const &,
         uint32_t = 0, uint32_t = 0
     ) noexcept;
+
+    void renderIS(obj::sphere const& s, obj::vect< float, 3 > const & view, image const& em, brdf::model brdf, obj::vect<uint32_t, 2> const* samples, uint32_t nb_samples);
 
     ~image( void ) noexcept;
 
