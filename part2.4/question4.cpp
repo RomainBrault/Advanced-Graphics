82c1ec272bae090efffab9459afbba6a5f48ab81
Romain Brault <romain@rb-fedora.workstation>
2013-02-27 03:16:08 +0000

    merged

--- part2.4/question4.cpp
+++ part2.4/question4.cpp
@@ -26,7 +26,7 @@ int main(int argc, char** argv) {
     }
 
     image latlong;
-    image temp(img_size, img_size, 3);
+    image temp;
 
     sphere s( img_size / 2, img_size / 2, img_size / 2 );
     vect< float, 3 > view( 0, 0, 1 );
@@ -36,27 +36,12 @@ int main(int argc, char** argv) {
         return -1;
     }
     rnd::Uniform< float > rng( time( nullptr ) );
-<<<<<<< HEAD
     brdf::model b( view, 1.0, 0.0, 1.0 );
     r_sphere.render( s, latlong, view, n_points, b, rng );
 
     temp = r_sphere;
     temp.linearToneMap( stops );
     temp.gamma( gamma );
-=======
-    obj::vect< uint32_t, 2 >* points = latlong.sampleEM( n_points, rng );
-    if ( points == nullptr ) {
-        return -1;
-    }
-
-    /* Add code here. */
-
-    temp.renderIS(s, view, latlong, brdf::model(view, 1.0, 0.0, 1.0), points, n_points);
-
-    temp.linearToneMap( 7 );
-    temp.gamma( 2.2 );
-
->>>>>>> a9dd86aed033eae61db0711bd32c8f3e1a7faaeb
     temp.normalise( 255 );
     temp.savePNM( "diffuseprobe.ppm", SFMT );
     return 0;
